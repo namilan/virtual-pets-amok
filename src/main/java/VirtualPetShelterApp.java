b16c7ad8d3308b1306d65033a1e233c4d85457ed
namilan <nichole.milan@gmail.com>
2019-06-05 23:44:42 -0400

    added more in my VPShelterApp

--- src/main/java/VirtualPetShelterApp.java
+++ src/main/java/VirtualPetShelterApp.java
@@ -1,9 +1,33 @@
+import java.util.List;
+import java.util.Scanner;
 
 public class VirtualPetShelterApp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Scanner input = new Scanner(System.in);
+		
+		VirtualPetShelter ronnysPlayhouse = new VirtualPetShelter();
+		VirtualPetShelter robotsPlayhouse = new VirtualPetShelter();
+		List<VirtualPet> organicsInPlayhouse = ronnysPlayhouse.getOrganicsInPlayhouse();
+		List<VirtualPet> robotsInPlayhouse = robotsPlayhouse.getRobotsInPlayhouse();
+		
+		VirtualPet ronny = new VirtualPet("Ronny");
+		organicsInPlayhouse.add(ronny);
+		VirtualPet kitteh = new VirtualPet("Kitteh");
+		organicsInPlayhouse.add(kitteh);
+		VirtualPet bork = new VirtualPet("Bork");
+		robotsInPlayhouse.add(bork);
+		VirtualPet meowth = new VirtualPet("Meowth");
+		robotsInPlayhouse.add(meowth);
+		
+		
 
+
+		
+		
+		
+		input.close();
 	}
 
 }
